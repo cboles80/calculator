09b091d03d81132a4041be70327a26bfd495f813
Chris Boles <cboles80@gmail.com>
2021-11-10 08:30:17 -0800

    Added event listeners and styled calculator

--- script.js
+++ script.js
@@ -1,4 +1,31 @@
-const version = "0.0.1a";
+const version = "0.0.2";
 const elVersion = document.querySelector('.version');
 console.log('Hello World ' + version);
 elVersion.innerHTML = version;
+
+const elKeys = document.querySelectorAll('.keyboard-frame div');
+const elDisplay = document.querySelector('.display')
+
+
+function handleUserInput(e) {
+    console.log('user input');
+    console.log(e);
+    console.log(e.type);
+    let myValue;
+    if (e.type === 'keyup'){
+        myValue = e.key;
+    } else{
+        console.log('else');
+        myValue = e.target.innerHTML;
+    }
+    console.log(myValue);
+    elDisplay.innerText = myValue;
+}
+
+// ON PAGE LOAD
+
+elKeys.forEach(key => {
+    key.addEventListener('click', handleUserInput);
+})
+
+document.addEventListener('keyup', handleUserInput);
