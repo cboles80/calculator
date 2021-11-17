de1a5866e8f92f4eb70752d1008410d97aca5ffe
Chris Boles <cboles80@gmail.com>
2021-11-17 09:37:03 -0800

    Added to handleUserInput function for validating input and displaying full entries

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-const version = "0.0.2";
+const version = "0.0.3";
 const elVersion = document.querySelector('.version');
 console.log('Hello World ' + version);
 elVersion.innerHTML = version;
 
 const elKeys = document.querySelectorAll('.keyboard-frame div');
 const elDisplay = document.querySelector('.display')
+const arValidInputs = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.',
+'/', 'x', '-', '+', '='];
+let inputBuffer = '';
 
 
 function handleUserInput(e) {
@@ -19,7 +22,24 @@ function handleUserInput(e) {
         myValue = e.target.innerHTML;
     }
     console.log(myValue);
-    elDisplay.innerText = myValue;
+    let idx = arValidInputs.indexOf(myValue);
+    console.log(idx);
+    //Check if input is valid
+    if(idx === -1){
+        return; //<============== EARLY RETURN
+    }
+    if(idx <= 11){
+        inputBuffer += myValue;
+        elDisplay.innerText = inputBuffer;
+    }else if(idx === arValidInputs.length - 1){
+
+    }else {
+        inputBuffer += ' ' + myValue + ' ';
+        elDisplay.innerText = inputBuffer;
+    }
+    
+    myValue = Array.of(myValue);
+    console.log(myValue);
 }
 
 // ON PAGE LOAD
